2479ceda28b2209d135e0b7b23b1cddf894abe3c
Admin <admin@MacBook-Pro-2.local>
2016-11-21 11:03:52 +1100

    Styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117041237) do
+ActiveRecord::Schema.define(version: 20161117073336) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.string   "seller"
+    t.text     "desciption"
+    t.integer  "price"
+    t.boolean  "availability"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
